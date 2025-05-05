1590863140f7b7a2595a12425c9b7d4317f8a008
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-05-05 19:44:01 +0100

    custom action 4 bookings

--- js/voiceflow.js
+++ js/voiceflow.js
@@ -16,6 +16,7 @@ import { CalExtension } from './extensions.js';
         userID: userId || 'USER_000',
         user: {
           name: 'UE_Guest',
+          image: 'https://ik.imagekit.io/bushera/profile%20images/02.png?updatedAt=1746351651791'
         },
         render: {
           mode: 'overlay',
