8e56dd96e8c9d0958a900d5b884e29645779af26
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-06-03 07:39:54 +0100

    building the projects path

--- js/voiceflow.js
+++ js/voiceflow.js
@@ -6,6 +6,23 @@ let userId = 'UE_000';  // Default value for userId
 (function(d, t) {
   const v = d.createElement(t), s = d.getElementsByTagName(t)[0];
 
+   function setTimer(initialDelay, newMessage, finalDelay) {
+      window.voiceflow.chat.proactive.push(
+        { type: 'text', payload: { message: 'Ellie and other supports are online and ready to handle your request 🔥🔥' } },
+        { type: 'text', payload: { message: 'Chat and book a session with one of our experts !' } }
+      );
+      setTimeout(() => {
+        window.voiceflow.chat.proactive.clear();
+        window.voiceflow.chat.proactive.push({
+          type: 'text', payload: { message: newMessage },
+        });
+        setTimeout(() => {
+          window.voiceflow.chat.proactive.clear();
+          window.voiceflow.chat.open();
+        }, finalDelay);
+      }, initialDelay);
+    };
+
   // Wait for userIdentified event before loading Voiceflow
 
     v.onload = function() {
@@ -41,24 +58,6 @@ let userId = 'UE_000';  // Default value for userId
         setTimer(2000, 'Ellie taking over the conversation right now !.......', 800);
       }
     });
-
-    function setTimer(initialDelay, newMessage, finalDelay) {
-      window.voiceflow.chat.proactive.push(
-        { type: 'text', payload: { message: 'Ellie and other supports are online and ready to handle your request 🔥🔥' } },
-        { type: 'text', payload: { message: 'Chat and book a session with one of our experts !' } }
-      );
-      setTimeout(() => {
-        window.voiceflow.chat.proactive.clear();
-        window.voiceflow.chat.proactive.push({
-          type: 'text', payload: { message: newMessage },
-        });
-        setTimeout(() => {
-          window.voiceflow.chat.proactive.clear();
-          window.voiceflow.chat.open();
-        }, finalDelay);
-      }, initialDelay);
-    };
-
   }
   v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
 })(document, 'script');
