c6f017f14b3889ea3104c05eec280611f1c55181
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-06-03 15:34:39 +0100

    add the project dashboard

--- js/extensions.js
+++ js/extensions.js
@@ -882,6 +882,8 @@ export const ProjectDashboardExtension = {
         document.body.appendChild(script);
       });
 
+       await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js");
+      await loadScript("https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js");
 
 
 
