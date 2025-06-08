e97b09ceeea9e380d7743afc23fc8db9d7ec8447
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-06-08 19:55:41 +0100

    fixing the voiceflow js

--- js/airtable.js
+++ js/airtable.js
@@ -108,7 +108,7 @@ document.addEventListener("DOMContentLoaded", async function () {
               Status: "New User",
               Website: websiteDomain,
               Page_URL: currentPage,
-              Visit_history: `Page visit:\n${currentPageEntry}`
+              Visit_history: 'Page visit:' + currentPage,
           },
       };
       await createAirtableRecord(newUserData, airtableApiKey, airtableBaseId, airtableTableName);
