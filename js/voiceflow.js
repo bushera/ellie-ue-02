085162c52b215a5231798a863b99243e8f9643b6
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-06-14 18:30:11 +0100

    code cleaning

--- js/voiceflow.js
+++ js/voiceflow.js
@@ -50,7 +50,7 @@ let userId = 'UE_000';  // Default value for userId
     }).then(() => {
       if (window.location.href.includes('https://elliepod.netlify.app/')) {
         window.voiceflow.chat.proactive.clear();
-        setProactive(10000, 'Ellie here, Lets  talk!.......', 3000);
+        setProactive(100, 'Ellie here, Lets  talk!.......', 5000);
       }
     });
 
