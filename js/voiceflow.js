ebe5ca5c9b42b93ee48205b21089e663e5a20509
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-06-15 21:35:40 +0100

    code cleaning

--- js/voiceflow.js
+++ js/voiceflow.js
@@ -50,7 +50,7 @@ let userId = 'UE_000';  // Default value for userId
     }).then(() => {
       if (window.location.href.includes('https://elliepod.netlify.app/')) {
         window.voiceflow.chat.proactive.clear();
-        setProactive(300, 'Toronto’s top digital agency, delivering up to 8X conversions—guaranteed.', 5000);
+        setProactive(100, 'Toronto’s top digital agency, delivering up to 8X conversions—guaranteed.', 10000);
       }
     });
 
