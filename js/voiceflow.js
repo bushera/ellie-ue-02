6efff0fcb4babf9401fbdffe87073affce533a34
Egwuh Adoyi Stanley <dollarbling@hotmail.com>
2025-06-18 14:31:39 +0100

    using btns to trigger VF flow

--- js/voiceflow.js
+++ js/voiceflow.js
@@ -59,11 +59,12 @@ let userId = 'UE_000';  // Default value for userId
 
           // Send the 'Open Chat' event to Voiceflow
           window.voiceflow.chat.interact({
-            
+            type: 'event',
+            payload: {
               event: {
                 name: 'openChatEvent' // The event name defined in your Event CMS
               }
-            
+            }
           });
         });
 
